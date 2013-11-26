0ba77e1af1e38a06aba85329da162e20594f8598
Fabian Jakobs <fabian@c9.io>
2013-11-26 11:47:34 +0000

    initial commit

---
+++ fontawesome.js
@@ -0,0 +1,40 @@
+/**
+ * This is an example of an almost minimal implementation of a plugin.
+ * Check out [the source](source/template_minimal.html)
+ *
+ * @class Template
+ * @extends Plugin
+ * @singleton
+ */
+define(function(require, exports, module) {
+    "use strict";    
+    main.consumes = ["Plugin", "ui"];
+    main.provides = ["fontawesome"];
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+        var ui     = imports.ui;
+        
+        /***** Initialization *****/
+        
+        var plugin = new Plugin("Ajax.org", main.consumes);
+        
+        var loaded = false;
+        function load() {
+            if (loaded) return false;
+            loaded = true;
+            
+            ui.insertCss(require("text!./css/font-awesome.css"), options.staticPrefix, plugin);
+        }
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("load", function(){
+            load();
+        });
+        
+        plugin.freezePublicAPI({});
+        register(null, { "fontawesome" : plugin });
+    }
+});
